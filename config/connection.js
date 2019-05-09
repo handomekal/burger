a1003ecf1803ce32fffe13f84f9851e4a406a689
handomekal <handomekal@gmail.com>
2019-05-09 16:45:04 -0400

    new env file made

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Maybachhab17!",
+  password: "process.env",
   database: "burgers_db"
 });
 
